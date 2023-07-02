e4614933076ecea8c8147ae41e4ad92ef8bf03e6
spartaMonster <changcailu@mail.ustc.edu.cn>
2023-07-02 15:34:45 +0800

    feat: Add logger for member

--- member/src/main/java/com/spartaMonster/train/member/config/MemberApplication.java
+++ member/src/main/java/com/spartaMonster/train/member/config/MemberApplication.java
@@ -1,16 +1,25 @@
 package com.spartaMonster.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 @ComponentScan("com.spartaMonster")
 //启动类默认只扫描当前包及子包下的组件，位于其他地方的需要开启扫描
 public class MemberApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+//        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("ADDRESS: http://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
