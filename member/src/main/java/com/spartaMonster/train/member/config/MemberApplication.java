ac05364bfbf77bc5c888e217d6f4e45420cd4502
spartaMonster <changcailu@mail.ustc.edu.cn>
2023-07-02 16:51:33 +0800

    feat: Add rest api test module with HTTPClient

--- member/src/main/java/com/spartaMonster/train/member/config/MemberApplication.java
+++ member/src/main/java/com/spartaMonster/train/member/config/MemberApplication.java
@@ -19,7 +19,7 @@ public class MemberApplication {
         SpringApplication app = new SpringApplication(MemberApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功");
-        LOG.info("ADDRESS: http://127.0.0.1:{}",env.getProperty("server.port"));
+        LOG.info("ADDRESS: http://127.0.0.1:{}{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
     }
 
 }
