84962454e19973fa88164faaa632442089b11aa0
spartaMonster <changcailu@mail.ustc.edu.cn>
2023-07-25 19:05:58 +0800

    feat: Log aop and req & resp VO

--- member/src/main/java/com/spartaMonster/train/member/controller/MemberController.java
+++ member/src/main/java/com/spartaMonster/train/member/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.spartaMonster.train.member.controller;
 
+import com.spartaMonster.train.common.resp.CommonResp;
+import com.spartaMonster.train.member.req.MemberRegisterReq;
 import com.spartaMonster.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,13 @@ public class MemberController {
     @GetMapping("/count")
     public Integer count(){
         return memberService.count();
+    }
+
+    @PostMapping("/register")
+    public CommonResp<Long> register(MemberRegisterReq req){
+        long register = memberService.register(req);
+        CommonResp<Long> resp = new CommonResp<>();
+        resp.setContent(register);
+        return resp;
     }
 }
--- member/src/main/java/com/spartaMonster/train/member/service/MemberService.java
+++ member/src/main/java/com/spartaMonster/train/member/service/MemberService.java
@@ -1,9 +1,15 @@
 package com.spartaMonster.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.spartaMonster.train.member.domain.Member;
+import com.spartaMonster.train.member.domain.MemberExample;
 import com.spartaMonster.train.member.mapper.MemberMapper;
+import com.spartaMonster.train.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -14,4 +20,18 @@ public class MemberService {
     public int count(){
         return Math.toIntExact(memberMapper.countByExample(null));
     };
+
+    public long register(MemberRegisterReq req){
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(req.getMobile());
+        List<Member> members = memberMapper.selectByExample(memberExample);
+        if(CollUtil.isNotEmpty(members)){
+            throw new RuntimeException("手机号已注册");
+        }
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(req.getMobile());
+        memberMapper.insert(member);
+        return member.getId();
+    }
 }
