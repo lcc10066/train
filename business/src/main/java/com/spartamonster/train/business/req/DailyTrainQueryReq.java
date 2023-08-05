a6a27900ec81551af78e8ff318639d17ddb8d0a8
spartaMonster <changcailu@mail.ustc.edu.cn>
2023-08-05 19:42:51 +0800

    note: Date class format diff in GET(@DateTimeFormat)&POST(JsonFormat)

--- business/src/main/java/com/spartamonster/train/business/req/DailyTrainQueryReq.java
+++ business/src/main/java/com/spartamonster/train/business/req/DailyTrainQueryReq.java
@@ -6,7 +6,15 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.util.Date;
 
 public class DailyTrainQueryReq extends PageReq {
-
+    /**
+     *
+     * GET请求使用
+     * @DateTimeFormat(pattern = "yyyy-MM-dd") 该注解将前端的请求参数映射为Date类型，指定格式，否则前端传来的字符串后端接口无法识别
+     *
+     *
+     * @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8") 只适用于POST请求
+     *
+     */
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
 
