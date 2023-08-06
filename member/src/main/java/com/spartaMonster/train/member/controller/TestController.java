45399165681bbe80361ca158729ff13ac728908d
spartaMonster <changcailu@mail.ustc.edu.cn>
2023-08-06 21:05:32 +0800

    feat: Read config XML

---
+++ member/src/main/java/com/spartaMonster/train/member/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.spartaMonster.train.member.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class TestController {
+    /**
+     *  读取配置文件配置的方式
+     *
+     *  如果想动态修改配置，可以基于Nacos来实现
+     */
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
+    @Autowired
+    Environment environment;
+
+    @GetMapping("/hello")
+    public String hello() {
+        String port = environment.getProperty("local.server.port");
+        return String.format("Hello %s! 端口：%s", testNacos, port);
+    }
+}
